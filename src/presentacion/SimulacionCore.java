66ec08d3f4bad6466e1d9b83d6552a6059fef08a
darthian <darthian@Darthian-PC.Sarasty>
2012-10-06 22:49:00 -0500

    implementacion botones

--- src/presentacion/SimulacionCore.java
+++ src/presentacion/SimulacionCore.java
@@ -1,14 +1,73 @@
 package presentacion;
 
-import processing.core.PApplet;
+import controlP5.ControlEvent;
+import processing.core.*;
+import controlP5.ControlP5;
 
 public class SimulacionCore extends PApplet {
 
+    public ControlP5 cp5 = null;
+    public int myColor = color(255);
+    public int c1, c2;
+    public float n, n1;
+
     public void setup() {
         size(800, 600);
-        background(48, 13, 250);
+        noStroke();
+        cp5 = new ControlP5(this);
+
+        // create a new button with name 'buttonA'
+        cp5.addButton("colorA")
+                .setValue(0)
+                .setPosition(100, 100)
+                .setSize(200, 19);
+
+        // and add another 2 buttons
+        cp5.addButton("colorB")
+                .setValue(100)
+                .setPosition(100, 120)
+                .setSize(200, 19);
+
+        cp5.addButton("colorC")
+                .setPosition(100, 140)
+                .setSize(200, 19)
+                .setValue(0);
+
+        
     }
 
     public void draw() {
+        background(myColor);
+        myColor = lerpColor(c1, c2, n);
+        n += (1 - n) * 0.1;
     }
+
+    public void controlEvent(ControlEvent theEvent) {
+        println(theEvent.getController().getName());
+        n = 0;
+    }
+
+// function colorA will receive changes from 
+// controller with name colorA
+    public void colorA(int theValue) {
+        println("a button event from colorA: " + theValue);
+        c1 = c2;
+        c2 = color(0, 160, 100);
+    }
+
+// function colorB will receive changes from 
+// controller with name colorB
+    public void colorB(int theValue) {
+        println("a button event from colorB: " + theValue);
+        c1 = c2;
+        c2 = color(150, 0, 0);
+    }
+
+// function colorC will receive changes from 
+// controller with name colorC
+    public void colorC(int theValue) {
+        println("a button event from colorC: " + theValue);
+        c1 = c2;
+        c2 = color(255, 255, 0);
+    }    
 }
--- src/presentacion/SimulacionIO2.java
+++ src/presentacion/SimulacionIO2.java
@@ -2,7 +2,7 @@ package presentacion;
 
 import processing.core.PApplet;
 
-public class SimulacionIO2 {
+public class SimulacionIO2 extends PApplet{
 
     public static void main(String[] args) {
         PApplet.main(new String[]{"presentacion.SimulacionCore"});
