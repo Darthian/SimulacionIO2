7dd5a1f1a33a030bf403cb26b8722013ba9a0135
darthian <darthian@Darthian-PC.Sarasty>
2012-10-07 14:46:06 -0500

    primer implementacion de botones de seleccion

--- src/presentacion/SimulacionCore.java
+++ src/presentacion/SimulacionCore.java
@@ -9,27 +9,48 @@ public class SimulacionCore extends PApplet {
     int myColor = color(255);
     int c1, c2;
     float n, n1;
+    RadioButton r;
 
     public void setup() {
-        size(800, 600);
+        size(1000, 650);
         noStroke();
         cp5 = new ControlP5(this);
 
-        // create a new button with name 'buttonA'
-        cp5.addButton("colorA")
+        r = cp5.addRadioButton("radioButton")
+                .setPosition(50, 100)
+                .setSize(40, 20)
+                .setColorForeground(color(120))
+                .setColorActive(color(255))
+                .setColorLabel(color(255))
+                .setItemsPerRow(1)
+                .setSpacingColumn(50)
+                .addItem("50", 1)
+                .addItem("100", 2)
+                .addItem("150", 3)
+                .addItem("200", 4)
+                .addItem("250", 5);
+
+        for (Toggle t : r.getItems()) {
+            t.captionLabel().setColorBackground(color(255, 80));
+            t.captionLabel().style().moveMargin(-7, 0, 0, -3);
+            t.captionLabel().style().movePadding(7, 0, 0, 3);
+            t.captionLabel().style().backgroundWidth = 45;
+            t.captionLabel().style().backgroundHeight = 13;
+        }
+        
+        cp5.addButton("Iniciar")
                 .setValue(0)
-                .setPosition(100, 100)
-                .setSize(200, 19);
+                .setPosition(100, 300)
+                .setSize(100, 19);
 
-        // and add another 2 buttons
-        cp5.addButton("colorB")
+        cp5.addButton("Detener")
                 .setValue(100)
-                .setPosition(100, 120)
-                .setSize(200, 19);
+                .setPosition(100, 320)
+                .setSize(100, 19);
 
-        cp5.addButton("colorC")
-                .setPosition(100, 140)
-                .setSize(200, 19)
+        cp5.addButton("Defecto")
+                .setPosition(100, 340)
+                .setSize(100, 19)
                 .setValue(0);
     }
 
@@ -39,32 +60,48 @@ public class SimulacionCore extends PApplet {
         n += (1 - n) * 0.1;
     }
 
-    public void controlEvent(ControlEvent theEvent) {
-        println(theEvent.getController().getName());
-        n = 0;
-    }
-
-// function colorA will receive changes from 
-// controller with name colorA
-    public void colorA(int theValue) {
+    public void Iniciar(int theValue) {
         println("a button event from colorA: " + theValue);
         c1 = c2;
         c2 = color(0, 160, 100);
     }
 
-// function colorB will receive changes from 
-// controller with name colorB
-    public void colorB(int theValue) {
+    public void Detener(int theValue) {
         println("a button event from colorB: " + theValue);
         c1 = c2;
         c2 = color(150, 0, 0);
     }
 
-// function colorC will receive changes from 
-// controller with name colorC
-    public void colorC(int theValue) {
+    public void Defecto(int theValue) {
         println("a button event from colorC: " + theValue);
         c1 = c2;
-        c2 = color(255, 255, 0);
+        c2 = color(0, 150, 150);
+    }
+
+    public void keyPressed() {
+        switch (key) {
+            case ('0'):
+                r.deactivateAll();
+                break;
+            case ('1'):
+                r.activate(0);
+                break;
+            case ('2'):
+                r.activate(1);
+                break;
+            case ('3'):
+                r.activate(2);
+                break;
+            case ('4'):
+                r.activate(3);
+                break;
+            case ('5'):
+                r.activate(4);
+                break;
+        }
+    }
+
+    public void radioButton(int a) {
+        println("a radio Button event: " + a);
     }
 }
